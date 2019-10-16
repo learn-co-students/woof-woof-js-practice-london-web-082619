04fc5f09e91aa7f11d5fabcf475d1c00db1123ea
github username <github email address>
2019-10-16 10:09:09 +0100

    Done.

---
+++ src/api.js
@@ -0,0 +1,34 @@
+
+// CONSTANTS
+
+const baseURL = "http://localhost:3000/pups/"
+const headers = {
+    "Content-Type": "application/json",
+    Accept: "application/json"
+}
+
+// API FUNCTIONALITY
+
+function get(){
+    return fetch(baseURL).then(resp => resp.json())
+}
+
+function patch(pupId, dataChange){
+    return fetch(baseURL+`${pupId}`, {
+        method: "PATCH",
+        headers: headers,
+        body: JSON.stringify({
+            isGoodDog: dataChange
+        })
+    })
+    .then(resp => resp.json())
+}
+
+const API = {
+    get: get(),
+    // patch: patch()
+}
+
+
+// two ways for the patch input:
+    // just reverse; look what there is and put opposite (can be done within fetch?)
